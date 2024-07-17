b7ab929c13d1a1862ca8f94bc2eb1da7a1e22c20
jeden1102 <bum123@o2.pl>
2024-07-17 22:17:37 +0200

    Init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,227 @@
-import Image from "next/image";
+"use client";
 
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 export default function Home() {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [value, setValue] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const handleClick = () => {
+    console.log("900729|2107|1988|45|0407|1507|1912");
+    const valid = "9007292107198845040715071912";
+    if (value === valid) {
+      console.log("ok");
+      return;
+    }
+    setIsError(true);
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+      <Card>
+        {currentStep === 0 && (
+          <>
+            <CardHeader>
+              <CardTitle>Sto lat, sto lat !</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Droga Madziu....</p>
+            </CardContent>
+            <CardFooter>
+              <Button onClick={() => setCurrentStep(1)}>Przejdz dalej</Button>
+            </CardFooter>
+          </>
+        )}
+        {currentStep === 1 && (
+          <>
+            <CardHeader>
+              <CardTitle>Quiz</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="mb-4">Opis...</p>
+              <div className="flex justify-center">
+                <Carousel className="w-full max-w-xs">
+                  <CarouselContent>
+                    <CarouselItem>
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex aspect-square items-center justify-center p-6 relative">
+                            <span className="absolute left-4 top-4">#1</span>
+                            <span className="text-xl font-semibold">
+                              ...tak zaczyna sie moj pesel.
+                            </span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                    <CarouselItem>
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex aspect-square items-center justify-center p-6 relative">
+                            <span className="absolute left-4 top-4">#2</span>
+                            <span className="text-xl font-semibold">
+                              W kalendarzu data ta błyszczy jak złoto, W letnim
+                              słońcu dzień wyjątkowy i ciepły tak oto. ?
+                              miesiąc, liczba magiczna ?, To czas, gdy
+                              świętujemy, w sercu radość pierwsza.
+                            </span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                    <CarouselItem>
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex flex-col gap-4 aspect-square items-center justify-center p-6 relative">
+                            <span className="absolute left-4 top-4">#3</span>
+                            <audio controls>
+                              <source src="/in-cold-light.mp3" />
+                            </audio>
+                            <span className="text-xl font-semibold">
+                              *podpowiedz - <b>rok</b>
+                            </span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                    <CarouselItem>
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex flex-col gap-4 aspect-square items-center justify-center p-6 relative">
+                            <span className="absolute left-4 top-4">#4</span>
+                            <span className="text-xl font-semibold">
+                              Wymiar ochronny krola polskich wod
+                            </span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                    <CarouselItem>
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex flex-col gap-4 aspect-square items-center justify-center p-6 relative">
+                            <span className="absolute left-4 top-4">#4</span>
+                            <audio controls>
+                              <source src="/4th.mp3" />
+                            </audio>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                    handleClick
+                    <CarouselItem>
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex flex-col gap-4 aspect-square items-center justify-center p-6 relative">
+                            <span className="absolute left-4 top-4">#5</span>
+                            <audio controls>
+                              <source src="/15ty.mp3" />
+                            </audio>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                    <CarouselItem>
+                      <div className="p-1">
+                        <Card>
+                          <CardContent className="flex flex-col gap-4 aspect-square items-center justify-center p-6 relative">
+                            <span className="absolute left-4 top-4">#6</span>
+                            <audio controls>
+                              <source src="/tita.mp3" />
+                            </audio>
+                            <span className="text-xl font-semibold">
+                              *podpowiedz - <b>rok</b>
+                            </span>
+                          </CardContent>
+                        </Card>
+                      </div>
+                    </CarouselItem>
+                  </CarouselContent>
+                  <CarouselPrevious />
+                  <CarouselNext />
+                </Carousel>
+              </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+              <div className="flex my-4 justify-center">
+                <InputOTP maxLength={32} onChange={(value) => setValue(value)}>
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={6} />
+                    <InputOTPSlot index={7} />
+                    <InputOTPSlot index={8} />
+                    <InputOTPSlot index={9} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={10} />
+                    <InputOTPSlot index={11} />
+                    <InputOTPSlot index={12} />
+                    <InputOTPSlot index={13} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={14} />
+                    <InputOTPSlot index={15} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={16} />
+                    <InputOTPSlot index={17} />
+                    <InputOTPSlot index={18} />
+                    <InputOTPSlot index={19} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={20} />
+                    <InputOTPSlot index={21} />
+                    <InputOTPSlot index={22} />
+                    <InputOTPSlot index={23} />
+                  </InputOTPGroup>
+                  <InputOTPSeparator />
+                  <InputOTPGroup>
+                    <InputOTPSlot index={24} />
+                    <InputOTPSlot index={25} />
+                    <InputOTPSlot index={26} />
+                    <InputOTPSlot index={27} />
+                  </InputOTPGroup>
+                </InputOTP>
+              </div>
+              <Button onClick={() => handleClick()}>Sprawdz kod</Button>
+              {isError && <p className="text-red-500">Nieprawidlowy numer</p>}
+            </CardContent>
+          </>
+        )}
+      </Card>
     </main>
   );
 }
